bbc8de1888b84b61281c951579f196a1ddfc5b74
Oliver Panasewicz <olipachi@gmail.com>
2015-11-04 15:38:53 -0600

    added documentation to ScaledPoint and UIWindow classes

--- ScaledPoint.java
+++ ScaledPoint.java
@@ -1,27 +1,42 @@
-public class ScaledPoint {
-	private int realWidth;
-	private int realHeight;
+//Class for scaling/resizing the elements on the application
 
-	public ScaledPoint() {
-		realWidth = 0;
-		realHeight = 0;
+public class ScaledPoint 
+{
+	private int realWidth;	//int for width of frame
+	private int realHeight;	//int for height of frame
+
+	//default constructor 
+	public ScaledPoint() 
+	{
+		realWidth = 0;		//set width to 0	
+		realHeight = 0;		//set height to 0
 	}
 
-	public ScaledPoint(int width, int height) {
-		realWidth = width;
-		realHeight = height;
+	public ScaledPoint(int width, int height)
+	//PRE: width and height are positive values 
+	{
+		realWidth = width;		//set width to frame width
+		realHeight = height;	//set height to frame height
 	}
 
-	public void setSize(int width, int height) {
-		realWidth = width;
-		realHeight = height;
+	public void setSize(int width, int height) 
+	//PRE: width and height are positive values
+	{
+		realWidth = width;		//set frame width 
+		realHeight = height;	//set frame height
 	}
 
-	public int[] getRealCoordinance(int x, int y) {
-		return new int[] {realWidth * x, realHeight * y};
+	public int[] getRealCoordinance(int x, int y) 
+	//PRE: x and y are positive values 
+	//POST: FCTVAL = integer array holding real x and y coordinates
+	{
+		return new int[] {realWidth * x, realHeight * y}; 	//return array of coordinates
 	}
 
-	public int[] getScaledCoordinance(int x, int y) {
-		return new int[] {x / realWidth, y / realHeight};
+	public int[] getScaledCoordinance(int x, int y) 
+	//PRE: x and y are positive values
+	//POST: FCTVAL = integer array holding scaled x and y coordinates
+	{
+		return new int[] {x / realWidth, y / realHeight};	//return array of coordinates
 	}
 }
